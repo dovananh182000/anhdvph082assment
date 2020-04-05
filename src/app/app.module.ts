499402d86f1e767f94ce4e6d3ef01c82fc9d8c36
dovananh182000 <anhdvph08201@fpt.edu.vn>
2020-04-06 00:55:36 +0700

    2 files modified, 68 added, 68 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
-import{ Content } from '.content/content.component';
+import{ ContentComponent } from "./content/content.component";
+import{ FooterComponent } from "./footer/footer.component";
+import{ HeaderBottomComponent } from "./header-bottom/header-bottom.component";
+import{ HeaderSildeComponent } from "./header-silde/header-silde.component";
+import{ HeaderTopComponent } from "./header-top/header-top.component";
+import{ HeadertopDescComponent } from "./headertop-desc/headertop-desc.component";
+
+
+
+
 // import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  imports: [BrowserModule, FormsModule,Content],
-  declarations: [AppComponent, HelloComponent],
+  imports: [BrowserModule, FormsModule,HttpClientModule],
+  declarations: [AppComponent, HelloComponent,ContentComponent,FooterComponent,HeaderBottomComponent,HeaderSildeComponent,HeaderTopComponent,HeadertopDescComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
